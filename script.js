f04b5a7127b65c1904739c965d70f4e5caa9c89f
DOEHOONLEE <DavidL@DOEs-MacBook.local>
2016-09-18 21:47:13 -0700

    <update> - 2.3
    - showing the possible moves on the instruction page
    - instruction message added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var hi = 'test'
 function allowDrop(ev) {
     ev.preventDefault();
 };
@@ -35,4 +34,73 @@ $(window).on("resize", function() {
        $('.navbar').show();
        $('.fa-bars').hide();
    }
+});
+$(document).ready(function() {
+    $('#red_cow').mouseover(function() {
+        $('#col_1_2, #col_2_1').addClass('highlight_red');
+    })
+    $('#red_cow').mouseleave(function() {
+        $('#col_1_2, #col_2_1').removeClass('highlight_red');
+    })
+});
+$(document).ready(function() {
+    $('#red_lion').mouseover(function() {
+        $('#col_1_1, #col_1_3, #col_2_2').addClass('highlight_red');
+    })
+    $('#red_lion').mouseleave(function() {
+        $('#col_1_1, #col_1_3, #col_2_2').removeClass('highlight_red');
+    })
+});
+$(document).ready(function() {
+    $('#red_rabbit').mouseover(function() {
+        $('#col_2_2').addClass('highlight_red');
+    })
+    $('#red_rabbit').mouseleave(function() {
+        $('#col_2_2').removeClass('highlight_red');
+    })
+});
+$(document).ready(function() {
+    $('#red_sheep').mouseover(function() {
+        $('#col_3_2').addClass('highlight_red');
+    })
+    $('#red_sheep').mouseleave(function() {
+        $('#col_3_2').removeClass('highlight_red');
+    })
+});
+$(document).ready(function() {
+    $('#blue_cow').mouseover(function() {
+        $('#col_3_3, #col_4_2').addClass('highlight_blue');
+    })
+    $('#blue_cow').mouseleave(function() {
+        $('#col_3_3, #col_4_2').removeClass('highlight_blue');
+    })
+});
+$(document).ready(function() {
+    $('#blue_tiger').mouseover(function() {
+        $('#col_3_2, #col_4_1, #col_4_3').addClass('highlight_blue');
+    })
+    $('#blue_tiger').mouseleave(function() {
+        $('#col_3_2, #col_4_1, #col_4_3').removeClass('highlight_blue');
+    })
+});
+$(document).ready(function() {
+    $('#blue_rabbit').mouseover(function() {
+        $('#col_3_2').addClass('highlight_blue');
+    })
+    $('#blue_rabbit').mouseleave(function() {
+        $('#col_3_2').removeClass('highlight_blue');
+    })
+});
+$(document).ready(function() {
+    $('#blue_sheep').mouseover(function() {
+        $('#col_2_2').addClass('highlight_blue');
+    })
+    $('#blue_sheep').mouseleave(function() {
+        $('#col_2_2').removeClass('highlight_blue');
+    })
+});
+$(document).ready(function() {
+    $('button').click(function() {
+        alert('If you bring your mouse onto the animal you would like to play, it will show the possible movements!');
+    })
 });
